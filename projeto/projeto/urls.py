8d1c8ff3a94df43a42b99dc76c5c01c3b13565cd
ArthurLux07 <marques.b.arthur@gmail.com>
2025-11-27 17:20:19 -0300

    inicio da config do forms

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from app.views import home
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro_usuario'),
 ]
